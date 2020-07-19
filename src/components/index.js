27556e5e7db823346d77fb25165f620f8ba1b37c
William Baumgartner <baumgartner.william@gmail.com>
2020-07-19 10:58:20 +0200

    chore: redux setup 2

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import AttackIndicator from './AttackIndicator/AttackIndicator';
+import Character from './Character/Character';
+import Dice from './Dice/Dice';
+import HealthBar from './HealthBar/HealthBar';
+
+export {
+  AttackIndicator,
+  Character,
+  Dice,
+  HealthBar
+}
