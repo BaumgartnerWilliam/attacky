e3b251a8f40b05ed077d0c43d3f844461cc37a94
William Baumgartner <baumgartner.william@gmail.com>
2020-07-19 11:35:04 +0200

    feat: redux middlewares

--- src/store.js
+++ src/store.js
@@ -1,15 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import initialState from './reducers';
 
 // todo: setup env.config.js
 // for simplicity this can remain here
 const isDev = process.env.NODE_ENV === 'development';
+const composeEnhancer = (isDev && composeWithDevTools) || compose;
+
+const middlewares = [isDev ? logger : undefined, thunk];
 
 export const store = createStore(
   initialState,
-  isDev ? composeWithDevTools() : undefined
+  composeEnhancer(applyMiddleware(...middlewares))
 );
 
 export default store;
